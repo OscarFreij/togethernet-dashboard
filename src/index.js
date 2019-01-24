6bb63ddc2e7dc21649f4118cb4e3421a561114a1
Movitz Sunar <movitz.sunar@gmail.com>
2019-01-24 12:12:54 +0100

    removed "hack of the year"

--- src/index.js
+++ src/index.js
@@ -6,15 +6,3 @@ const vm = new Vue({
   el: '#root',
   render: h => h(App),
 });
-
-// Hack of the year
-window.vm = vm;
-
-const adminWindow = open();
-adminWindow.document.body.innerHTML = '<div id="root"></div>';
-new Vue({
-  el: adminWindow.document.getElementById('root'),
-  render: h => h(Admin),
-});
-
-window.addEventListener('beforeunload', () => { adminWindow.close(); });
